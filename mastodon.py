d499bdbe03fda86a4b625769e163549cbd032c1b
Michael Morisy <m@morisy.com>
2023-11-03 15:58:33 -0400

    Update mastodon.py

--- mastodon.py
+++ mastodon.py
@@ -4,7 +4,7 @@ from mastodon import Mastodon
 # Load Mastodon API credentials
 mastodon = Mastodon(
     access_token='your_access_token',
-    api_base_url='https://mastodon.social'  # Replace with your instance
+    api_base_url='https://botsin.space'  # Replace with your instance
 )
 
 # Read the lines from the text file
